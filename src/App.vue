92b9e59ba5329bc2342e6e015eea3221cdc21a76
luucasor <lucas@gatilabs.com.br>
2018-09-02 22:25:23 -0300

    Criação de componente Painel.vue

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,28 @@
     <h1 class="centralizado">{{ titulo }}</h1>
     <ul class="lista-fotos">
       <li class="lista-fotos-item" v-for="foto in fotos">
-        <img :src="foto.url" :alt="foto.titulo"/>
+        
+        <meu-painel :titulo='foto.titulo'>
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo"/>
+        </meu-painel>
+
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import Painel from './components/shared/painel/Painel.vue';
+
 export default {
+  components: {
+    'meu-painel': Painel  
+  },
+
   data(){
     return {
       titulo: 'Alurapic',
-      fotos: [
-
-      ]
+      fotos: []
     }
   },
   created(){
@@ -28,21 +36,25 @@ export default {
 </script>
 
 <style>
-.corpo {
-  font-family: Helvetica, sans-serif;
-  width: 96%;
-  margin: 0 auto;
-}
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+  }
 
-.centralizado {
-  text-align: center;
-}
+  .centralizado {
+    text-align: center;
+  }
 
-.lista-fotos {
-  list-style: none;
-}
+  .lista-fotos {
+    list-style: none;
+  }
 
-.lista-fotos .lista-fotos-item {
-  display: inline-block;
-}
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .imagem-responsiva {
+    width: 100%;
+  }
 </style>
